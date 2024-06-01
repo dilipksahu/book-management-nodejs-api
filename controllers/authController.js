3f13ad943bb2471920a1f580959844f16d4ea0c4
dilipksahu <sahud048@gmail.com>
2024-06-01 21:56:16 +0530

    auth login created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,38 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.register = async (req, res) => {
-  
-    const usernameExists = await User.findOne({ username: req.body.username });
-    if (usernameExists) return res.status(400).send({success: false, message: 'Username already exists'});
-  
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password
-    });
+  const usernameExists = await User.findOne({ username: req.body.username });
+  if (usernameExists)
+    return res
+      .status(400)
+      .send({ success: false, message: "Username already exists" });
 
-    try {
-        const savedUser = await user.save();
-        res.send({ success: true, message: "Register successfully", user: user._id });
-      } catch (error) {
-        res.status(400).send({success:false,  error});
-      }
-    };
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send({
+      success: true,
+      message: "Register successfully",
+      user: user._id,
+    });
+  } catch (error) {
+    res.status(400).send({ success: false, error });
+  }
+};
+
+exports.login = async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (!user || !req.body.password) return res.status(400).send({ success: false, message:"Username or password is wrong"});
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send({ success: false, message:"Invalid password"});
+
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  res.header("auth-token", token).send({ success: true, access_token:token});
+};
